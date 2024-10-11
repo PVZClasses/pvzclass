6517551599c6fd3288d06e0f65106fe4b3ba5929
Violet Nonbloosom <86547296+Violet-Nonbloosom@users.noreply.github.com>
2024-10-11 21:52:32 +0800

    Broaden Savedata

--- pvzclass/Classes/SaveData.cpp
+++ pvzclass/Classes/SaveData.cpp
@@ -4,7 +4,6 @@ void PVZ::SaveData::GetPVZUserName(char str[])
 {
 	Memory::ReadArray<char>(BaseAddress + 4, str, 12);
 }
-
 int PVZ::SaveData::GetChallengeRecord(PVZLevel::PVZLevel mode)
 {
 	return Memory::ReadMemory<int>(BaseAddress + 0x30 + (mode - 1) * 4);
@@ -13,6 +12,16 @@ int PVZ::SaveData::GetChallengeRecord(PVZLevel::PVZLevel mode)
 void PVZ::SaveData::SetChallengeRecord(PVZLevel::PVZLevel mode, int val)
 {
 	Memory::WriteMemory<int>(BaseAddress + 0x30 + (mode - 1) * 4, val);
+}
+
+int PVZ::SaveData::GetPurchase(StoreItem::StoreItem item)
+{
+	return Memory::ReadMemory<int>(BaseAddress + 0x1C0 + item * 4);
+}
+
+void PVZ::SaveData::SetPurchase(StoreItem::StoreItem item, int val)
+{
+	Memory::WriteMemory<int>(BaseAddress + 0x1C0 + item * 4, val);
 }
 
 BOOLEAN PVZ::SaveData::HavePurpleCard(CardType::CardType purplecard)

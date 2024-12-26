8c8fd2e689490b4f30ef319ab18c6985026202c0
Violet Nonbloosom <86547296+Violet-Nonbloosom@users.noreply.github.com>
2024-12-26 20:54:07 +0800

    PlantEatenEvent bug fix.

--- pvzclass/Events/PlantEatenEvent.hpp
+++ pvzclass/Events/PlantEatenEvent.hpp
@@ -3,7 +3,7 @@
 
 // 植物被啃死事件。
 // @param 依次为：触发事件的植物、啃食该植物的僵尸。
-// @return 是否跳过原版的结算过程（即不被啃死）。
+// @return 是否继续结算原版的结算过程。
 class PlantEatenEvent : public DLLEvent
 {
 public:
@@ -20,7 +20,13 @@ PlantEatenEvent::PlantEatenEvent()
 		PUSH_EBP,
 		PUSH_ESI,
 		INVOKE(procAddress),
-		ADD_ESP(8)
+		ADD_ESP(8),
+
+		TEST_AL_AL,
+		JNZ(6),
+		POPAD,
+		PUSHDWORD(0x52FDEE),
+		RET
 	};
 	start(STRING(code));
 }

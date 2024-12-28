ea01fee56fe0e91d1eb0fbcd0416759562ebbcbf
Violet Nonbloosom <86547296+Violet-Nonbloosom@users.noreply.github.com>
2024-12-28 09:49:46 +0800

    VaseOpenEvent derive from DLLEvent

--- pvzclass/Events/VaseOpenEvent.h
+++ pvzclass/Events/VaseOpenEvent.h
@@ -1,15 +1,31 @@
 ﻿#pragma once
-#include "TemplateEvent.h"
+#include "DLLEvent.h"
 
-// 开罐子事件。
-// 多个事件之间罐子会串联修改。
-// @param 触发事件的罐子、是否取消该事件。
-// @return 更新后被打开的罐子。
-class VaseOpenEvent : public TemplateEvent<std::function<
-	SPT<PVZ::Vase>(SPT<PVZ::Vase>, bool&)> >
+// 开罐事件。
+// 时机上优先级高于开罐的其他操作。
+// @param 依次为：触发事件的 Miscellaneous、触发该事件的罐子基址。
+// @return 是否继续结算罐子被砸开的事件。返回“否”会阻止罐子被敲开。
+class VaseOpenEvent : public DLLEvent
 {
 public:
 	VaseOpenEvent();
-	void handle(CONTEXT& context) override;
 };
 
+VaseOpenEvent::VaseOpenEvent()
+{
+	int procAddress = PVZ::Memory::GetProcAddress("onVaseOpen");
+	hookAddress = 0x429AC0;
+	rawlen = 6;
+	BYTE code[] =
+	{
+		PUSH_PTR_ESP_ADD_V(0x28),
+		PUSH_PTR_ESP_ADD_V(0x28),
+		INVOKE(procAddress),
+		ADD_ESP(8),
+
+		TEST_AL_AL,
+		JNZ(3),
+		RETN(8)
+	};
+	start(STRING(code));
+}

8241c32dcfd06110f27cb5c6241a1818908bdf04
Violet Nonbloosom <86547296+Violet-Nonbloosom@users.noreply.github.com>
2024-12-29 08:45:28 +0800

    PlantAddProjectileEvent add param

--- pvzclass/Events/PlantAddProjectileEvent.hpp
+++ pvzclass/Events/PlantAddProjectileEvent.hpp
@@ -3,7 +3,7 @@
 
 // 植物发射子弹事件。
 // 时机上先于子弹索敌类型设定和特殊子弹速度改动。
-// @param 依次为：触发事件的植物、生成的子弹。
+// @param 依次为：触发事件的植物、生成的子弹、子弹目标僵尸的基址。
 // @return 是否继续结算原版的调整。
 class PlantAddProjectileEvent : public DLLEvent
 {
@@ -18,10 +18,11 @@ PlantAddProjectileEvent::PlantAddProjectileEvent()
 	rawlen = 6;
 	BYTE code[] =
 	{
+		PUSH_PTR_ESP_ADD_V(0x54),
 		PUSH_EAX,
 		PUSH_EBP,
 		INVOKE(procAddress),
-		ADD_ESP(8),
+		ADD_ESP(0x0C),
 
 		TEST_AL_AL,
 		JNZ(6),
